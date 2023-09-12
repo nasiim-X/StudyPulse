918ee32bda40e9b2d15395421bd7c7be0321a1f9
Nasiim X <nasiim.nsmlp@gmail.com>
2023-09-12 20:45:10 +0600

    template/main pages

--- StudyPulse/StudyPulse/urls.py
+++ StudyPulse/StudyPulse/urls.py
@@ -6,6 +6,12 @@ from .import views
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-
     path('base',views.BASE,name='base'),
+
+    path('', views.HOME, name='home'),
+    path('single/course', views.SINGLE_COURSE, name='single_course'),
+
+    path('contact',views.CONTACT,name='contact'),
+
+    path('about',views.ABOUT, name='about'),
 ]
