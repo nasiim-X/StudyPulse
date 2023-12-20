b243ed00a007f7cb2597f8539b7aab6828f61bd8
Nasiim X <nasiim.nsmlp@gmail.com>
2023-12-20 17:21:27 +0600

    profile update

--- StudyPulse/StudyPulse/settings.py
+++ StudyPulse/StudyPulse/settings.py
@@ -130,5 +130,5 @@ LOGOUT_REDIRECT_URL = 'login'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'email'
-EMAIL_HOST_PASSWORD = 'password'
+EMAIL_HOST_USER = 'nasiim.nsmlp@gmail.com'
+EMAIL_HOST_PASSWORD = 'dfkfkjfdkfdk45463jfnasiim'
--- StudyPulse/StudyPulse/urls.py
+++ StudyPulse/StudyPulse/urls.py
@@ -18,5 +18,7 @@ urlpatterns = [
     path('accounts/register', user_login.REGISTER, name='register'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('doLogin', user_login.DO_LOGIN, name='doLogin'),
+    path('accounts/profile', user_login.PROFILE, name='profile'),
+    path('accounts/profile/update', user_login.PROFILE_UPDATE, name='profile_update'),
 
 ]
