f2c6039731fb283671620d30c27efbdf5ed3d5cc
Nasiim X <nasiim.nsmlp@gmail.com>
2023-12-22 05:37:36 +0600

    lesson & vdo model

--- StudyPulse/app/admin.py
+++ StudyPulse/app/admin.py
@@ -8,8 +8,11 @@ class what_you_learn_TabularInline(admin.TabularInline):
 class Requirements_TabularInline(admin.TabularInline):
     model = Requirements
 
+class Video_TabularInline(admin.TabularInline):
+    model = Video
+
 class course_admin(admin.ModelAdmin):
-    inlines = (what_you_learn_TabularInline,Requirements_TabularInline)
+    inlines = (what_you_learn_TabularInline,Requirements_TabularInline,Video_TabularInline)
 
 admin.site.register(Categories)
 admin.site.register(Author)
@@ -17,3 +20,5 @@ admin.site.register(Course,course_admin)
 admin.site.register(Level)
 admin.site.register(What_you_learn)
 admin.site.register(Requirements)
+admin.site.register(Lesson)
+admin.site.register(Video)
